77c08754cbace78aaac3aa3276acfa1e5c9b6bdf
peterg7 <pgish97@gmail.com>
2021-12-07 11:34:14 -0800

    included connection rules

--- static/config/connections.js
+++ static/config/connections.js
@@ -2,7 +2,11 @@
  * Define different types of connections
  */
 var connectionTypes = {
-    
+    "CLIENT":["EDGE"],
+    "EDGE":["CLIENT", "PRE-PROCESSOR", "PROCESSOR", "ENDPOINT"],
+    "PRE-PROCESSOR":["PROCESSOR","EDGE"],
+    "PROCESSOR": ["EDGE","PRE-PROCESSOR","ENDPOINT", "PROCESSOR"],
+    "ENDPOINT": ["PROCESSOR", "EDGE"]
 };
 
 export default connectionTypes;
