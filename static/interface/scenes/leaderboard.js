7970070b50b225945bf2adf90a45e3bdd0cbe23a
Tiffany Zhang <48366808+Tiffanyxk3@users.noreply.github.com>
2021-12-08 22:46:04 +0800

    instruc and settings page fit all screen sizes

--- static/interface/scenes/leaderboard.js
+++ static/interface/scenes/leaderboard.js
@@ -30,15 +30,15 @@ Leaderboard.prototype.init = function() {
         'CLOUD_COMPUTE': 'I\'m a cloud-computing software  Cost: (L1)$0 (L2)$120\n\Tips: The starting point of the customer\'s request.',
       },
       'Processors': {
-        'GATEWAY': 'A Gateway is a hardware device that acts as a \'gate\' \n\ between two networks.',
-        'HUB': 'A hub is a device that allows multiple computers to communicate\n\ with each other over a network.',
-        'SWITCH': 'A switch is used to network multiple computers together. \n\ Unlike hubs, switches can limit the traffic to and from each port.',
-        'MODEM': 'Modem is a hardware component that allows a computer or another device, \n\ such as a router or switch, to connect to the Internet.',
-        'ROUTER': 'Router is a hardware device that routes data from a local \n\ area network (LAN) to another network connection.',
-        'LOAD_BALANCER': 'A load balancer is a piece of hardware that acts\n\ like a reverse proxy to distribute network.',
-        'CACHE': 'A cache is a hardware or software component that stores data\n\ so that future requests for that data can be served faster.',
-        'SERVER': 'This device may connect over a network to a server on a different device.\n\ A server is a piece of computer hardware or software.',
-        'DATABASE': 'Organized collection of structured data. \n\ Online databases are hosted on websites. '
+        'GATEWAY': 'A Gateway is a hardware device that acts as a \'gate\' between two networks. It may be a \n\ router, firewall, server, or another device that enables traffic to flow in and out of the network.',
+        'HUB': 'A hub is a device that allows multiple computers to communicate with each other over a network. It has several Ethernet ports that are used to \n\ connect two or more network devices together.',
+        'SWITCH': 'A switch is used to network multiple computers together. Switches are more advanced than hubs and less capable than routers. Unlike hubs,\n\ switches can limit the traffic to and from each port so that each device connected to the switch has a sufficient amount of bandwidth.',
+        'MODEM': 'Modem is a hardware component that allows a computer or another device, such as a router or switch, to connect to the Internet.',
+        'ROUTER': 'Router is a hardware device that routes data from a local area network (LAN) to another network connection. A router acts like a coin sorting machine, allowing only authorized machines to connect to other computer systems.',
+        'LOAD_BALANCER': 'A load balancer is a piece of hardware that acts like a reverse proxy to distribute network and application traffic across different servers.\n\It is used to improve the concurrent user capacity and overall reliability of applications.',
+        'CACHE': 'A cache is a hardware or software component that stores data so that future requests for that data can be served faster. The data stored in a cache might be the result of an\n\ earlier computation or a copy of data stored elsewhere.',
+        'SERVER': 'This device may connect over a network to a server on a different device. A server is a piece of computer hardware or software that provides functionality for other programs\n\ or devices, called \'clients\'. ',
+        'DATABASE': 'Organized collection of structured data. Online databases are hosted on websites. made available as software as a service products accessible via a web browser.'
       }
     }
 
@@ -62,7 +62,7 @@ Leaderboard.prototype.init = function() {
 
     this.gapHeight = 100 // the distance from title to the 1st subtitle 
     this.subTitleCount = 0 
-    this.subTitleBottomMargin = 50
+    this.subTitleBottomMargin = 40
 
     this.componentCount = 0
     this.componentBottomMargin = 60
@@ -83,16 +83,22 @@ Leaderboard.prototype.init = function() {
 
         // Based on Home Scene dimensions
         titleWidthRatio: 0.35, // ratio compared to the screen width
-        titleHeightRatio: 0.07, // ratio compared to the screen height
+        titleHeightRatio: 0.05, // ratio compared to the screen height
+        yTitleOffsetRatio: 0.5, // ratio of x-spacing from screen top based on title height
         yTitleOffsetRatio: 0.5, // ratio of y-spacing from screen top based on title height
 
         subTitleWidthRatio: 0.25, // ratio compared to the screen width
-        subTitleHeightRatio: 0.05, // ratio compared to the screen height
-        yTitleOffsetRatio: 0.5, // ratio of y-spacing from screen top based on title height
+        subTitleHeightRatio: 0.035, // ratio compared to the screen height
+
+        instrWidthRatio: 0.3, // ratio compared to the screen width
+        instrHeightRatio: 0.025, // ratio compared to the screen height
         
         controlIconScale: 0.45, // used to resize the control icons (home, volume, settings...)
         constrolIconXSpacerRatio: 0.35, // spacing ratio based on scaled icon height
         controlIconOpacity: k.opacity(0.9), // opacity of control-enabled icons
+
+        spriteIconScale: 0.1,
+        spriteIconMarginScale: 0.15,
 
         board: {
             // (Same as Home Scene Menu Box)
@@ -123,17 +129,30 @@ Leaderboard.prototype.init = function() {
     this.params['titleHeight'] = this.params.height * this.params.titleHeightRatio;
 
     // Calculated distance between top of title and top of screen
+    this.params['xSpacer'] = this.params.titleHeight * this.params.yTitleOffsetRatio;
     this.params['yTitleSpacer'] = this.params.titleHeight * this.params.yTitleOffsetRatio;
 
     this.objects = {};
     this.objects['title'] = {
         width: this.params.width * this.params.titleWidthRatio,
         height: this.params.height * this.params.titleHeightRatio,
-        x: ((this.params.width / 2) - (this.params.titleWidth / 2)),
-        y: (this.params.screenY + this.params.yTitleSpacer)
+        x: this.params.screenX + this.params.xSpacer,
+        y: this.params.screenY + this.params.yTitleSpacer
     }
 
+    this.objects['subtitle'] = {
+        width: this.params.width * this.params.titleWidthRatio,
+        height: this.params.height * this.params.subTitleHeightRatio,
+        x: this.params.screenX + this.params.xSpacer,
+        y: this.params.screenY + this.params.yTitleSpacer
+    }
 
+    this.objects['instruction'] = {
+        width: this.params.width * this.params.instrWidthRatio,
+        height: this.params.height * this.params.instrHeightRatio,
+        x: this.params.screenX + this.params.xSpacer,
+        y: this.params.screenY + this.params.yTitleSpacer
+    }
 
     this.objects['back'] = { 
         x: ((this.params.width + this.params.screenX) // right-most edge
@@ -145,10 +164,11 @@ Leaderboard.prototype.init = function() {
 
 
 Leaderboard.prototype.buildScene = function(color) {
+    // TODO: fix this image imports
     // load sprites
-    for (let name in this.componentsImg){
-      k.loadSprite(name, this.componentsImg[name])
-    }
+    // for (let name in this.componentsImg){
+    //   k.loadSprite(name, this.componentsImg[name])
+    // }
 
     // Backdrop color
     k.add([
@@ -159,31 +179,37 @@ Leaderboard.prototype.buildScene = function(color) {
     ]);
 
     // Title
-    k.add([
-        k.text('Instructions', { size: this.objects.title.height - 30, width: this.objects.title.width }),
-        k.pos(this.objects.title.x - 200, this.objects.title.y-10),
-    ]);
+    // k.add([
+    //     k.text('Instructions', { size: this.objects.title.height, width: this.objects.title.width }),
+    //     k.pos(this.objects.title.x, this.objects.title.y),
+    // ]);
 
     for (let type in this.components){
       // add subtitle(type)
       k.add([
-        k.text(type, { size: this.objects.title.height - 45 }),
-        k.pos(this.objects.title.x - 175, this.objects.title.y-70 + this.gapHeight + this.subTitleBottomMargin*this.subTitleCount + this.componentCount*this.componentBottomMargin),
+        k.text(type, { size: this.objects.subtitle.height }),
+        // k.pos(this.objects.subtitle.x, this.objects.subtitle.y + this.gapHeight + this.subTitleBottomMargin * this.subTitleCount + this.componentCount * this.componentBottomMargin),
+        k.pos(this.objects.subtitle.x, 
+          this.objects.subtitle.y + this.subTitleBottomMargin * this.subTitleCount + this.componentCount * this.componentBottomMargin),
       ])
       // addsubTitleCount
       this.subTitleCount++
       for (let componentName in this.components[type]){
         // add componentImg
-        k.add([
-          k.scale(0.1),
+        const spr = k.add([
+          k.scale(this.params.spriteIconScale),
           k.sprite(componentName),
-          k.pos(this.objects.title.x - 150, this.objects.title.y-80  + this.gapHeight + this.subTitleBottomMargin*this.subTitleCount + this.componentCount*this.componentBottomMargin),
+          // k.pos(this.objects.title.x, this.objects.title.y  + this.gapHeight + this.subTitleBottomMargin*this.subTitleCount + this.componentCount*this.componentBottomMargin),
+          k.pos(this.objects.title.x, 
+            this.objects.title.y + this.subTitleBottomMargin * this.subTitleCount + this.componentCount * this.componentBottomMargin),
         ])
         // add componentName and info 
         k.add([
           k.color(0,0,100),
-          k.text(`${componentName}:${this.components[type][componentName]}`,{ size: this.objects.title.height - 55 }),
-          k.pos(this.objects.title.x - 50, this.objects.title.y-70 + this.gapHeight + this.subTitleBottomMargin*this.subTitleCount + this.componentCount*this.componentBottomMargin),
+          k.text(`${componentName}:${this.components[type][componentName]}`, { size: this.objects.instruction.height, width: this.params.width }),
+          // k.pos(this.objects.title.x + 100, this.objects.title.y + this.gapHeight + this.subTitleBottomMargin*this.subTitleCount + this.componentCount*this.componentBottomMargin),
+          k.pos(this.objects.title.x + spr.width * this.params.spriteIconMarginScale, 
+            this.objects.title.y + this.subTitleBottomMargin * this.subTitleCount + this.componentCount * this.componentBottomMargin),
         ])
         this.componentCount++
       }
@@ -192,14 +218,16 @@ Leaderboard.prototype.buildScene = function(color) {
 
     // Back button
     const backBtn = k.add([
-      k.text('Back', { size: this.objects.title.height - 40}),
-      k.pos(this.objects.title.x + 400, this.objects.title.y-80+ this.gapHeight + this.subTitleBottomMargin*this.subTitleCount + this.componentCount*this.componentBottomMargin),
+      k.text('Back', { size: this.objects.title.height}),
+      k.pos(this.params.width * 0.98, this.objects.title.y),
+        // this.objects.title.y + this.gapHeight + this.subTitleBottomMargin * this.subTitleCount + this.componentCount * this.componentBottomMargin),
       k.color(0,0,100),
       k.area(),
-
+      k.origin("right"),
     ]);
+
+    backBtn.hovers(() => { backBtn.scale = 1.1; }, () => { backBtn.scale = 1; });
     backBtn.clicks(SceneControls.goHome);
-    this.init();
 };
 
 
