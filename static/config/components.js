96bfe7ab3efbec49da7c46a7355e483f2d2628a9
Ray-popo <rma17@dons.usfca.edu>
2021-12-09 12:55:22 -0800

    Update components.js

--- static/config/components.js
+++ static/config/components.js
@@ -126,7 +126,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 1, // 1 coin/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 3, // can have 3 outgoing connections
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 3 // can process 3 requests per "latency interval"
@@ -134,7 +134,7 @@ export const componentDefs = {
                 2: {
                     cost: 50, // cost to upgrade
                     usageCost: 2, // 2 coins/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 4, // can have 4 outgoing connections
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 4 // can process 4 requests per "latency interval"
@@ -151,7 +151,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 1, // 1 coin/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 2, // can have 2 outgoing connections
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 3 // can process 3 requests per "latency interval"
@@ -159,7 +159,7 @@ export const componentDefs = {
                 2: {
                     cost: 50, // cost to upgrade
                     usageCost: 2, // 2 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 3, // can have 3 outgoing connections
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 4 // can process 4 requests per "latency interval"
@@ -177,7 +177,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 2, // 2 coins/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 2, // can have 2 outgoing connections
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 4 // can process 4 requests per "latency interval"
@@ -185,7 +185,7 @@ export const componentDefs = {
                 2: {
                     cost: 50, // cost to upgrade
                     usageCost: 3, // 3 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 3, // can have 3 outgoing connections
                     latency: 1, // takes 1 second to process a single request
                     throughput: 5 // can process 5 requests per "latency interval"
@@ -205,7 +205,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 3, // 3 coins/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 4, // can have 4 outgoing connections
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 5 // can process 5 requests per "latency interval"
@@ -213,7 +213,7 @@ export const componentDefs = {
                 2: {
                     cost: 100, // cost to upgrade
                     usageCost: 5, // 5 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 4, // can have 4 outgoing connections
                     latency: 1, // takes 1 second to process a single request
                     throughput: 7 // can process 7 requests per "latency interval"
@@ -229,7 +229,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 3, // 3 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 4, // can have 4 outgoing connections
                     latency: 2, // takes 1.5 seconds to process a single request
                     throughput: 5 // can process 5 requests per "latency interval"
@@ -237,7 +237,7 @@ export const componentDefs = {
                 2: {
                     cost: 80, // cost to upgrade
                     usageCost: 4, // 4 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 4, // can have 4 outgoing connections
                     latency: 1, // takes 1 second to process a single request
                     throughput: 6 // can process 6 requests per "latency interval"
@@ -255,7 +255,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 2, // 2 coins/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 3, // can have 3 outgoing connections
                     latency: 1, // takes 1 seconds to process a single request
                     throughput: 6 // can process 6 requests per "latency interval"
@@ -263,7 +263,7 @@ export const componentDefs = {
                 2: {
                     cost: 70, // cost to upgrade
                     usageCost: 4, // 4 coins/sec
-                    maxInputs: 4, // can have 4 incoming connections
+                    maxInputs: 999, // can have 4 incoming connections
                     maxOutputs: 4, // can have 4 outgoing connections
                     latency: 1, // takes 0.5 seconds to process a single request
                     throughput: 10 // can process 10 requests per "latency interval"
@@ -280,15 +280,15 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 2, // 2 coins/sec
-                    maxInputs: 1, // can have 1 incoming connection
-                    maxOutputs: 1, // can have 1 outgoing connection
+                    maxInputs: 999, // can have 1 incoming connection
+                    maxOutputs: 999, // can have 1 outgoing connection
                     latency: 1, // takes 1 second to process a single request
                     throughput: 8 // can process 8 requests per "latency interval"
                 },
                 2: {
                     cost: 90, // cost to upgrade
                     usageCost: 4, // 4 coins/sec
-                    maxInputs: 2, // can have 2 incoming connections
+                    maxInputs: 999, // can have 2 incoming connections
                     maxOutputs: 2, // can have 2 outgoing connections
                     latency: 1, // takes 0.5 seconds to process a single request
                     throughput: 15 // can process 15 requests per "latency interval"
@@ -305,7 +305,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 1, // 1 coin/sec
-                    maxInputs: 2, // can have 2 incoming connection
+                    maxInputs: 999, // can have 2 incoming connection
                     maxOutputs: 2, // can have 2 outgoing connection
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 5 // can process 5 requests per "latency interval"
@@ -313,7 +313,7 @@ export const componentDefs = {
                 2: {
                     cost: 70, // cost to upgrade
                     usageCost: 3, // 3 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 3, // can have 3 outgoing connections
                     latency: 1, // takes 1.5 seconds to process a single request
                     throughput: 8 // can process 8 requests per "latency interval"
@@ -331,7 +331,7 @@ export const componentDefs = {
                 1: {
                     cost: 0, // base level component is free (charged for usage)
                     usageCost: 1, // 1 coin/sec
-                    maxInputs: 2, // can have 2 incoming connection
+                    maxInputs: 999, // can have 2 incoming connection
                     maxOutputs: 2, // can have 2 outgoing connection
                     latency: 2, // takes 2 seconds to process a single request
                     throughput: 5 // can process 5 requests per "latency interval"
@@ -339,7 +339,7 @@ export const componentDefs = {
                 2: {
                     cost: 80, // cost to upgrade
                     usageCost: 3, // 3 coins/sec
-                    maxInputs: 3, // can have 3 incoming connections
+                    maxInputs: 999, // can have 3 incoming connections
                     maxOutputs: 3, // can have 3 outgoing connections
                     latency: 1, // takes 1.5 seconds to process a single request
                     throughput: 8 // can process 8 requests per "latency interval"
